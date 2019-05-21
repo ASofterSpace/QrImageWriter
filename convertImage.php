58a0d486fc5d918ac3ac7ccd561bb408e8711469
Tom Moya Schiller <moya@asofterspace.com>
2019-05-22 01:00:26 +0200

    Improve converter php script

--- convertImage.php
+++ convertImage.php
@@ -6,8 +6,9 @@
 <?php
 
 	$extension = pathinfo($_FILES['picture']['name'], PATHINFO_EXTENSION);
+	$file_identifier = pathinfo($_FILES['picture']['name'], PATHINFO_FILENAME);
 
-	$input_path = '/var/www/html/asofterspaceen/qrimagewriter/converter/input.' . $extension;
+	$input_path = '/var/www/html/asofterspaceen/qrimagewriter/converter/input_' . $file_identifier . '.' . $extension;
 
 	move_uploaded_file($_FILES['picture']['tmp_name'], $input_path);
 
@@ -25,9 +26,13 @@
 
 	exec("/var/www/html/asofterspaceen/qrimagewriter/converter/run.sh");
 
-?>
+	$output_name = 'output_' . $file_identifier . '.png';
+	
+	rename("/var/www/html/asofterspaceen/qrimagewriter/converter/output.png", "/var/www/html/asofterspaceen/qrimagewriter/converter/" . $output_name);
 
-Here is your picture:<br><br>
-<img src="https://www.asofterspace.com/qrimagewriter/converter/output.png" />
+	echo("Here is your picture:<br><br>");
+	echo('<img src="https://www.asofterspace.com/qrimagewriter/converter/' . $output_name . '" />');
+	
+?>
 
 </body>
